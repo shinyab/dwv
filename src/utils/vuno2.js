185cf421b523cd9b5c225cec71004fa2c4c9aa3b
신승영 <shinyab@bluehack.net>
2017-09-27 10:34:33 +0900

    apply indent

---
+++ src/utils/vuno2.js
@@ -0,0 +1,23 @@
+// namespaces
+var dwv = dwv || {};
+dwv.utils = dwv.utils || {};
+/** @namespace */
+dwv.utils.base = dwv.utils.base || {};
+
+dwv.utils.load = function (uri) {
+  console.log(uri);
+
+};
+
+// "use strict";
+//
+// var fs = require('fs');
+//
+// exports.Vuno = function() {
+//
+//   function loadDicom(path) {
+//
+//     var dicomParser = new dwv.dicom.DicomParser();
+//   };
+//
+// };
--- src/vuno/vuno.js
+++ src/vuno/vuno.js
@@ -1,12 +1,16 @@
-// "use strict";
-//
-// var fs = require('fs');
-//
-// exports.Vuno = function() {
-//
-//   function loadDicom(path) {
-//
-//     var dicomParser = new dwv.dicom.DicomParser();
-//   };
-//
-// };
+
+var fs = require('fs');
+
+// namespaces
+var dwv = dwv || {};
+dwv.vuno = dwv.vuno || {};
+/** @namespace */
+dwv.vuno.base = dwv.vuno.base || {};
+
+dwv.vuno.load = function (url) {
+
+  fs.readFile(url, 'utf8', function(err, contents) {
+
+    console.log(contents);
+  });
+};
